cc8090e46debe98481263faa7c2b786ddcfda4f2
Jojodicus <jojodicus@gmail.com>
2022-01-28 00:51:02 +0100

    Added TODOs

--- recess.c
+++ recess.c
@@ -1,13 +1,15 @@
 #define _GNU_SOURCE
 
+// TODO: clean up includes
 #include <dlfcn.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <time.h>
 
 void *malloc(size_t size) {
-    srand(time(0));
+    srand(time(0)); // this should not belong here
 
+    // TODO: make chance customizable
     if (!(rand() % 2)) {
         errno = ENOMEM;
         return NULL;
@@ -16,3 +18,5 @@ void *malloc(size_t size) {
     void *(*orig_malloc)(size_t) = dlsym(RTLD_NEXT, "malloc");
     return orig_malloc(size);
 }
+
+// TODO: add lots of other syscalls
