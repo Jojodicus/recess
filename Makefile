d28c3d5535290282edda950e566d43cf9cec7a1c
Jojodicus <jojodicus@gmail.com>
2022-02-01 01:42:36 +0100

    tried customization

--- Makefile
+++ Makefile
@@ -1,23 +1,30 @@
 CC = gcc
+CFLAGS = -Wall -g -O2 -std=c11
+
 SRC = recess
+LIB = lib$(SRC).so
+CFG = $(SRC).cfg
+
 RM = rm
 INSTALL_PATH = $(HOME)/.local/bin
+CFG_PATH = $(HOME)/.config
 
-all: lib$(SRC).so
+all: $(LIB)
 
 clean:
 	$(RM) -f *.so
 
 install: all
 	mkdir -p $(INSTALL_PATH)
+	mkdir -p $(CFG_PATH)
 	cp $(SRC) $(INSTALL_PATH)/$(SRC)
-	cp lib$(SRC).so $(INSTALL_PATH)/lib$(SRC).so
+	cp $(LIB) $(INSTALL_PATH)/$(LIB)
 	chmod +x $(INSTALL_PATH)/$(SRC)
 
 uninstall:
-	rm -rf $(INSTALL_PATH)/$(SRC) $(INSTALL_PATH)/lib$(SRC).so
+	rm -rf $(INSTALL_PATH)/$(SRC) $(INSTALL_PATH)/$(LIB) $(CFG_PATH)/$(CFG)
 
-lib%.so: %.c
+$(LIB): $(SRC).c
 	$(CC) -shared -fPIC -ldl $< -o $@
 
 .PHONY: all clean install uninstall
--- recess.c
+++ recess.c
@@ -3,14 +3,115 @@
 // TODO: clean up includes
 #include <dlfcn.h>
 #include <errno.h>
+#include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 
-void *malloc(size_t size) {
-    srand(time(0)); // this should not belong here
+#ifndef CFG_PATH
+#define CFG_PATH "/.config/recess.cfg"
+#endif
 
-    // TODO: make chance customizable
-    if (!(rand() % 2)) {
+#define UNINIT 0xFF
+#define INITIN 0xFE
+#define DEFAULT_CHANCE 100
+#define RECESS_INIT if (g_chance == UNINIT) { recess_init(); }
+
+// error chance
+static volatile u_int8_t g_chance = UNINIT;
+
+// TODO: remove debug statements
+void recess_init(void) {
+    // initializing
+    g_chance = INITIN;
+
+    // set random seed
+    srand(time(NULL));
+
+    fprintf(stderr, "Loading config file...\n");
+
+    // get home environment variable
+    char *home = getenv("HOME");
+    if (home == NULL) {
+        // oh oh
+        fprintf(stderr, "Error: HOME environment variable not set. The heck are you doing?\n");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr, "41\n");
+    // build path to config file
+    size_t home_len = strlen(home);
+    fprintf(stderr, "44\n");
+    size_t cfg_len = strlen(CFG_PATH);
+    fprintf(stderr, "46\n");
+    char *cfg_path = malloc(home_len + cfg_len + 1); // segfaults cuz stack overflow ahhh
+    if (cfg_path == NULL) {
+        // oh oh
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    fprintf(stderr, "53\n");
+    strcpy(cfg_path, home);
+    strcat(cfg_path, CFG_PATH);
+    fprintf(stderr, "56\n");
+
+    // open config file
+    FILE *fp = fopen(cfg_path, "r");
+    if (fp == NULL) {
+        fprintf(stderr, "failed to open config file, using default chance\n");
+        g_chance = DEFAULT_CHANCE;
+        return;
+    }
+
+    // read chance
+    char buf[16];
+    if (fgets(buf, sizeof(buf), fp) == NULL) {
+        fprintf(stderr, "failed to read config file, using default chance\n");
+        g_chance = DEFAULT_CHANCE;
+        return;
+    }
+
+    // parse chance from format chance=%d
+    char *p = strchr(buf, '=');
+    if (p == NULL) {
+        fprintf(stderr, "failed to parse config file, using default chance\n");
+        g_chance = DEFAULT_CHANCE;
+    }
+    p++;
+    int chance = atoi(p);
+
+    // check range
+    if (chance < 0) {
+        fprintf(stderr, "invalid chance %d, clamping to 0\n", chance);
+        g_chance = 0;
+        return;
+    }
+    if (chance > 100) {
+        fprintf(stderr, "invalid chance %d, clamping to 100\n", chance);
+        g_chance = 100;
+        return;
+    }
+
+    // set chance
+    g_chance = chance;
+}
+
+// could be changed a macro
+static bool should_fail(void) {
+    // dont break initialization's syscalls
+    if (g_chance == INITIN) {
+        return false;
+    }
+
+    return (rand() % 100) < g_chance;
+}
+
+// example
+void *malloc(size_t size) {
+    RECESS_INIT
+
+    if (should_fail()) {
+        fprintf(stderr, "malloc failed!\n");
         errno = ENOMEM;
         return NULL;
     }
