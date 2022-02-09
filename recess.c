4fa1b32d0771714d12221ef6531df7ef520eea35
Jojodicus <jojodicus@gmail.com>
2022-02-09 23:40:18 +0100

    added config parser

--- recess.c
+++ recess.c
@@ -9,17 +9,128 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 #include <dlfcn.h>
 
-#ifndef FAIL_CHANCE
-#define FAIL_CHANCE 50
+// config will be in $HOME/.config/recess.cfg
+#define CONFIG_NAME "/.config/recess.cfg"
+
+#ifndef DEFAULT_FAIL_CHANCE
+#define DEFAULT_FAIL_CHANCE 10
 #endif
 
 // flag if rng has been seeded so we don't seed it more than necessary
-static bool _g_rand_init = false;
+static int _g_fail_chance = -1;
 // flag if failures are currently suppressed, accessible from outside
 bool g_recess_suppressed = false;
+
+static int _get_config_path(char **path) {
+    // get home directory
+    char *home = getenv("HOME");
+    if (home == NULL) {
+        return -1;
+    }
+
+    // allocate memory for path
+    *path = malloc(strlen(home) + strlen(CONFIG_NAME) + 1);
+    if (*path == NULL) {
+        return -1;
+    }
+
+    // build path
+    strncpy(*path, home, strlen(home) + 1);
+    strncat(*path, CONFIG_NAME, strlen(CONFIG_NAME) + 1);
+
+    return 0;
+}
+
+// simple config parser
+static void _parse_fail_chance() {
+    // suppress shims
+    g_recess_suppressed = true;
+
+    // get config path
+    char *path;
+    if (_get_config_path(&path) != 0) {
+        fprintf(stderr, "recess: failed to get config path\n");
+        _g_fail_chance = DEFAULT_FAIL_CHANCE;
+        return;
+    }
+
+    // open config file
+    FILE *config = fopen(path, "r");
+    free(path); // don't need path anymore
+    if (config == NULL) {
+        perror("recess - failed to open config file");
+        _g_fail_chance = DEFAULT_FAIL_CHANCE;
+        return;
+    }
+
+    // read config file
+    char *line = NULL;
+    size_t len = 0;
+    u_int32_t line_num = 0;
+    long fail_chance = -1;
+    while (getline(&line, &len, config) != -1) {
+        line_num++;
+
+        // skip comments
+        if (line[0] == '#') {
+            continue;
+        }
+
+        // parse line
+        char *key = strtok(line, "=");
+        char *value = strtok(NULL, "=");
+
+        // check for valid key
+        if (strcmp(key, "FAIL_CHANCE") != 0) {
+            continue;
+        }
+
+        // check for valid value
+        if (value == NULL) {
+            fprintf(stderr, "recess - invalid value for FAIL_CHANCE at line %d, searching for valid value\n", line_num);
+            continue;
+        }
+
+        // parse value
+        fail_chance = strtol(value, NULL, 10);
+
+        // check for valid range
+        if (fail_chance < 0 || fail_chance > 100) {
+            fprintf(stderr, "recess - invalid value for FAIL_CHANCE at line %d, searching for valid value\n", line_num);
+            continue;
+        }
+
+        // set fail chance
+        _g_fail_chance = (int) fail_chance;
+        break;
+    }
+
+    // check for errors
+    if (ferror(config)) {
+        perror("recess - error while reading config file");
+        _g_fail_chance = DEFAULT_FAIL_CHANCE;
+        return;
+    }
+
+    // close config file
+    fclose(config);
+
+    // free line
+    free(line);
+
+    // check if we found a valid value
+    if (_g_fail_chance == -1) {
+        fprintf(stderr, "recess - failed to find valid value for FAIL_CHANCE, using default value\n");
+        _g_fail_chance = DEFAULT_FAIL_CHANCE;
+    }
+
+    // unsuppress failures
+    g_recess_suppressed = false;
+}
 
 static bool _should_fail(){
     // suppressed -> no failures
@@ -28,13 +139,13 @@ static bool _should_fail(){
     }
 
     // initialize rng if necessary
-    if (!_g_rand_init){
+    if (_g_fail_chance == -1) {
         srand(time(NULL));
-        _g_rand_init = true;
+        _parse_fail_chance();
     }
 
     // roll
-    return rand() % 100 < FAIL_CHANCE;
+    return rand() % 100 < _g_fail_chance;
 }
 
 
