d83258f9f7c7959169b79e669820aac1a3690de5
Jojodicus <jojodicus@gmail.com>
2022-02-26 15:22:58 +0100

    add header guard

--- src/recess.h
+++ src/recess.h
@@ -1,6 +1,8 @@
 // @NOTE: It is not recommended to use this header, as recess is an *optional* dynamic library.
 //        Making a program rely on this header will cause very weird errors when the dynamic linker cannot find the library.
 //        The header is only provided for convenience/testing purposes.
+#ifndef RECESS_H
+#define RECESS_H
 
 #include <stdbool.h>
 
@@ -9,3 +11,4 @@
  */
 extern bool g_recess_suppressed;
 
+#endif // RECESS_H
