97b066efc4416f58f7e8a821d74a36905d15c8af
jojodicus <jojodicus@gmail.com>
2022-05-24 09:57:30 +0200

    formatting

--- src/config_utils.c
+++ src/config_utils.c
@@ -3,6 +3,7 @@
 #include "config_utils.h"
 
 // @TODO: clean up includes
+#include <dlfcn.h>
 #include <errno.h>
 #include <libconfig.h>
 #include <stdbool.h>
@@ -10,14 +11,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <dlfcn.h>
 
 #include "recess.h"
 
 static config_t *parsed_config = NULL;
 
-static void destroy_config()
-{
+static void destroy_config() {
     recess_suppressed = true;
     config_destroy(parsed_config);
     free(parsed_config);
@@ -25,33 +24,28 @@ static void destroy_config()
 }
 
 // @TODO XDG compliant
-static int get_config_path(char **path)
-{
+static int get_config_path(char **path) {
     // get home directory
     char *home = getenv("HOME");
-    if (home == NULL)
-    {
+    if (home == NULL) {
         return -1;
     }
 
     // allocate memory for path
     *path = malloc(strlen(home) + strlen(CONFIG_NAME) + 1);
-    if (*path == NULL)
-    {
+    if (*path == NULL) {
         return -1;
     }
 
     // build path
-    if (sprintf(*path, "%s/%s", home, CONFIG_NAME) < 0)
-    {
+    if (sprintf(*path, "%s/%s", home, CONFIG_NAME) < 0) {
         return -1;
     }
 
     return 0;
 }
 
-static void parse_config()
-{
+static void parse_config() {
     // suppress shims
     recess_suppressed = true;
 
@@ -63,8 +57,7 @@ static void parse_config()
 
     // get config path
     char *path;
-    if (get_config_path(&path) != 0)
-    {
+    if (get_config_path(&path) != 0) {
         fprintf(stderr, "recess: failed to get config path\n");
         recess_suppressed = false;
         return;
@@ -72,9 +65,9 @@ static void parse_config()
 
     // open config file
     config_init(parsed_config);
-    if (config_read_file(parsed_config, path) == CONFIG_FALSE)
-    {
-        fprintf(stderr, "recess: %s:%d - %s\n", config_error_file(parsed_config),
+    if (config_read_file(parsed_config, path) == CONFIG_FALSE) {
+        fprintf(
+            stderr, "recess: %s:%d - %s\n", config_error_file(parsed_config),
             config_error_line(parsed_config), config_error_text(parsed_config));
 
         // clear config
@@ -88,27 +81,22 @@ static void parse_config()
     recess_suppressed = false;
 }
 
-bool should_fail(const char *method)
-{
+bool should_fail(const char *method) {
     // suppressed -> no failures
-    if (recess_suppressed)
-    {
+    if (recess_suppressed) {
         return false;
     }
 
     // initialize rng if necessary
-    if (parsed_config == NULL)
-    {
+    if (parsed_config == NULL) {
         srand(time(NULL));
         parse_config();
     }
 
     // lookup
     int chance;
-    if (config_lookup_int(parsed_config, method, &chance) == CONFIG_FALSE)
-    {
-        if (config_lookup_int(parsed_config, "default", &chance) == CONFIG_FALSE)
-        {
+    if (config_lookup_int(parsed_config, method, &chance) == CONFIG_FALSE) {
+        if (config_lookup_int(parsed_config, "default", &chance) == CONFIG_FALSE) {
             chance = DEFAULT_FAIL_CHANCE;
         }
     }
